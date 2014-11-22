fcc72059655e0a8f108af35e1a90e617a9c9a776
zarghol <clement.nonn@gmail.com>
2014-11-22 20:05:06 +0100

    IBDesignable and LayoutConstraints

--- MYBlurIntroductionView/MYIntroductionPanel.swift
+++ MYBlurIntroductionView/MYIntroductionPanel.swift
@@ -12,136 +12,169 @@ import Foundation
 
 // File Panel
 // TODO: add to panel for Interface Builder
-let topPadding : CGFloat = 30.0
-let leftRightMargins : CGFloat = 10.0
-let bottomPadding : CGFloat = 48.0
-let headerTitlePadding : CGFloat = 10.0
-let titleDescriptionPadding : CGFloat = 10.0
-let descriptionImagePadding : CGFloat = 10.0
-
-var titleFont: UIFont = UIFont.boldSystemFontOfSize(21.0)
-var titleTextColor: UIColor = UIColor.whiteColor()
-var descriptionFont: UIFont = UIFont.systemFontOfSize(14.0)
-var descriptionTextColor: UIColor = UIColor.whiteColor()
-var separatorLineColor: UIColor = UIColor(white: 0, alpha: 0.1)
+//
 
 
 
-class MYIntroductionPanel : UIView {
-    //weak var parentIntroductionView: MYBlurIntroductionView?
-    private var panelHeaderView: UIView?
-    private(set)var panelTitle: String
-    private(set)var panelDescription: String
-    private var panelTitleLabel: UILabel
-    private var panelDescriptionLabel: UILabel
-    var panelSeparatorLine: UIView
-    private var panelImageView: UIImageView?
+@IBDesignable public class MYIntroductionPanel : UIView {
+    weak var parentIntroductionView: MYBlurIntroductionView?
     
-    private(set) var isCustomPanel: Bool
-    private(set) var hasCustomAnimation: Bool
+    var headerView: UIView
+    public var titleLabel: UILabel
+    var descriptionLabel: UILabel
+    public var separatorLine: UIView
+    var imageView: UIImageView
+    
+    @IBInspectable var titleFont: UIFont {
+        get {
+            return self.titleLabel.font
+        }
+        
+        set {
+            self.titleLabel.font = newValue
+        }
+    }
+    
+    @IBInspectable var titleTextColor: UIColor {
+        get {
+            return self.titleLabel.textColor
+        }
+        
+        set {
+            self.titleLabel.textColor = newValue
+        }
+    }
+    
+    @IBInspectable var descriptionFont: UIFont {
+        get {
+            return self.descriptionLabel.font
+        }
+        
+        set {
+            self.descriptionLabel.font = newValue
+        }
+    }
+    
+    @IBInspectable var descriptionTextColor: UIColor {
+        get {
+            return self.descriptionLabel.textColor
+        }
+        
+        set {
+            self.descriptionLabel.textColor = newValue
+        }
+    }
+    
+    @IBInspectable var separatorLineColor: UIColor {
+        get {
+            return self.separatorLine.backgroundColor ?? UIColor.clearColor()
+        }
+        
+        set {
+            self.separatorLine.backgroundColor = newValue
+        }
+    }
+    
+    @IBInspectable var topPadding : CGFloat = 30.0 {
+        didSet {
+            self.updateConstraints()
+        }
+    }
+    @IBInspectable var leftRightMargins : CGFloat = 10.0 {
+        didSet {
+            self.updateConstraints()
+        }
+    }
+    @IBInspectable var bottomPadding : CGFloat = 48.0 {
+        didSet {
+            self.updateConstraints()
+        }
+    }
+    @IBInspectable var headerTitlePadding : CGFloat = 10.0 {
+        didSet {
+            self.updateConstraints()
+        }
+    }
+    @IBInspectable var titleSeparatorPadding : CGFloat = 2.0 {
+        didSet {
+            self.updateConstraints()
+        }
+    }
+    @IBInspectable var descriptionImagePadding : CGFloat = 10.0 {
+        didSet {
+            self.updateConstraints()
+        }
+    }
+
+
+    
+    override convenience public init(frame: CGRect) {
+        
+        var header = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0))
+        header.backgroundColor = UIColor.yellowColor()
+        self.init(frame: frame, title: "Title", description: "description", image: UIImage(), headerView: header)
+    }
     
     
-    init(frame: CGRect, title: String, description: String, image: UIImage? = nil, headerView: UIView? = nil) {
+    public init(frame: CGRect, title: String, description: String, image: UIImage, headerView: UIView) {
         // initialize constant
         
-        self.panelHeaderView = headerView
-        self.panelTitle = title
-        self.panelDescription = description
-        self.isCustomPanel = false
-        self.hasCustomAnimation = false
-        if let img = image {
-            self.panelImageView = UIImageView(image: img)
-        }
-        self.panelTitleLabel = UILabel()
-        self.panelDescriptionLabel = UILabel()
-        self.panelSeparatorLine = UIView()
+        self.headerView = headerView
+        
+        self.imageView = UIImageView(image: image)
+        
+        self.titleLabel = UILabel()
+        self.titleLabel.text = title
+        
+        self.descriptionLabel = UILabel()
+        self.descriptionLabel.text = description
+        
+        self.separatorLine = UIView()
+        
         super.init(frame: frame)
-        self.buildPanelWithFrame(frame)
+        
+        self.titleFont = UIFont.boldSystemFontOfSize(21.0)
+        self.titleTextColor = UIColor.whiteColor()
+        
+        self.descriptionFont = UIFont.systemFontOfSize(14.0)
+        self.descriptionTextColor = UIColor.whiteColor()
+        
+        self.separatorLineColor = UIColor(white: 0, alpha: 0.1)
+                
+        self.buildPanel()
+        self.buildConstraints()
     }
-    
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+
+    required public init(coder aDecoder: NSCoder) {
+
+        self.headerView = UIView()
+        self.imageView = UIImageView()
+        self.titleLabel = UILabel()
+        self.descriptionLabel = UILabel()
+        self.separatorLine = UIView()
+
+        super.init(coder: aDecoder)
+        
+        self.titleFont = UIFont.boldSystemFontOfSize(21.0)
+        self.titleTextColor = UIColor.whiteColor()
+        
+        self.descriptionFont = UIFont.systemFontOfSize(14.0)
+        self.descriptionTextColor = UIColor.whiteColor()
+        
+        self.separatorLineColor = UIColor(white: 0, alpha: 0.1)
+        
+        
+        self.buildPanel()
+        self.buildConstraints()
     }
+
     
     class func loadWithNib(frame: CGRect, nibNamed nibName:String) -> MYIntroductionPanel! {
         
         var panel =  NSBundle.mainBundle().loadNibNamed(nibName, owner: nil, options: nil)[0] as MYIntroductionPanel
         panel.frame = frame
-        panel.isCustomPanel = true
-        panel.hasCustomAnimation = false
         
         return panel
-    }
-    
-    
-    func buildPanelWithFrame(frame: CGRect) {
-        
-        
-        var runningYOffset = topPadding
-        
-        //Process panel header view, if it exists
-        if var header = self.panelHeaderView {
-            header.frame = CGRect(x: (frame.size.width - header.frame.size.width) / 2, y: runningYOffset, width: header.frame.size.width, height: header.frame.size.height)
-            self.addSubview(header)
-            runningYOffset += header.frame.size.height + headerTitlePadding
-        }
-        
-        //Calculate title and description heights
-        var panelTitleHeight: CGFloat = 0.0
-        var panelDescriptionHeight: CGFloat = 0.0
-        
-        // title
-        let titleAttributes = NSDictionary(object: titleFont, forKey: NSFontAttributeName)
-        panelTitleHeight = NSString(string: self.panelTitle).boundingRectWithSize(CGSize(width: frame.size.width - 2 * leftRightMargins, height: CGFloat.infinity), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: titleAttributes, context: nil).size.height
-        
-        panelTitleHeight = ceil(panelTitleHeight)
-        
-        // description
-        let descriptionAttributes = NSDictionary(object: descriptionFont, forKey: NSFontAttributeName)
-        panelDescriptionHeight = NSString(string: self.panelDescription).boundingRectWithSize(CGSize(width: frame.size.width - 2 * leftRightMargins, height: CGFloat.infinity), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: descriptionAttributes, context: nil).size.height
-        
-        panelDescriptionHeight = ceil(panelDescriptionHeight)
-        
-        // Create titleLabel
-        self.panelTitleLabel = UILabel(frame: CGRect(x: leftRightMargins, y: runningYOffset, width: frame.size.width - 2 * leftRightMargins, height: panelTitleHeight))
-        self.panelTitleLabel.numberOfLines = 0
-        self.panelTitleLabel.text = self.panelTitle
-        self.panelTitleLabel.font = titleFont
-        self.panelTitleLabel.textColor = titleTextColor
-        self.panelTitleLabel.alpha = 0.0
-        self.panelTitleLabel.backgroundColor = UIColor.clearColor()
-        self.addSubview(self.panelTitleLabel)
-        
-        runningYOffset += panelTitleHeight + titleDescriptionPadding
-        
-        //Add small line in between title and description
-        self.panelSeparatorLine = UIView(frame: CGRect(x: leftRightMargins, y: runningYOffset - 0.5 * titleDescriptionPadding, width: frame.size.width - 2 * leftRightMargins, height: 1))
-        self.panelSeparatorLine.backgroundColor = separatorLineColor
-        self.addSubview(self.panelSeparatorLine)
-        
-        //Create description label
-        self.panelDescriptionLabel = UILabel(frame: CGRect(x: leftRightMargins, y: runningYOffset, width: frame.size.width - 2 * leftRightMargins, height: panelDescriptionHeight))
-        self.panelDescriptionLabel.numberOfLines = 0
-        self.panelDescriptionLabel.text = self.panelDescription
-        self.panelDescriptionLabel.font = descriptionFont
-        self.panelDescriptionLabel.textColor = descriptionTextColor
-        self.panelDescriptionLabel.alpha = 0.0
-        self.panelDescriptionLabel.backgroundColor = UIColor.clearColor()
-        self.addSubview(self.panelDescriptionLabel)
-        
-        runningYOffset += panelDescriptionHeight + descriptionImagePadding
-        
-        //Add image, if there is room
-        if var imageView = self.panelImageView {
-            imageView.frame = CGRect(x: leftRightMargins, y: runningYOffset, width: self.frame.size.width - 2 * leftRightMargins, height: self.frame.size.height - runningYOffset - bottomPadding)
-            imageView.contentMode = UIViewContentMode.Center
-            imageView.clipsToBounds = true
-            self.addSubview(imageView)
-        }
-        
-        if self.isCustomPanel {
-            self.hasCustomAnimation = true
-        }
     }
     
     func panelDidAppear() {
@@ -153,43 +186,138 @@ class MYIntroductionPanel : UIView {
     }
     
     func hideContent() {
-        self.panelTitleLabel.alpha = 0.0
-        self.panelDescriptionLabel.alpha = 0.0
-        self.panelSeparatorLine.alpha = 0.0
-        self.panelHeaderView?.alpha = 0.0
-        self.panelImageView?.alpha = 0.0
+        self.titleLabel.alpha = 0.0
+        self.descriptionLabel.alpha = 0.0
+        self.separatorLine.alpha = 0.0
+        self.headerView.alpha = 0.0
+        self.imageView.alpha = 0.0
     }
-    
+//
     func showContent() {
-        if isCustomPanel && !hasCustomAnimation {
-            return
-        }
-        
+//        if isCustomPanel && !hasCustomAnimation {
+//            return
+//        }
+//        
         //Get initial frames
-        var initialHeaderFrame = self.panelHeaderView?.frame ?? CGRectZero
-        var initialTitleFrame = self.panelTitleLabel.frame
-        var initialDescriptionFrame = self.panelDescriptionLabel.frame
-        var initialImageViewFrame = self.panelImageView?.frame ?? CGRectZero
+        var initialHeaderFrame = self.headerView.frame
+        var initialTitleFrame = self.titleLabel.frame
+        var initialDescriptionFrame = self.descriptionLabel.frame
+        var initialImageViewFrame = self.imageView.frame
         
-        self.panelTitleLabel.frame = initialTitleFrame.rectByOffsetting(dx: 10, dy: 0)
-        self.panelDescriptionLabel.frame = initialDescriptionFrame.rectByOffsetting(dx: 10, dy: 0)
-        self.panelHeaderView?.frame = initialHeaderFrame.rectByOffsetting(dx: 0, dy: -10)
-        self.panelImageView?.frame = initialImageViewFrame.rectByOffsetting(dx: 0, dy: 10)
+        self.titleLabel.frame = initialTitleFrame.rectByOffsetting(dx: 10, dy: 0)
+        self.descriptionLabel.frame = initialDescriptionFrame.rectByOffsetting(dx: 10, dy: 0)
+        self.headerView.frame = initialHeaderFrame.rectByOffsetting(dx: 0, dy: -10)
+        self.imageView.frame = initialImageViewFrame.rectByOffsetting(dx: 0, dy: 10)
         
         UIView.animateWithDuration(0.3, animations: {
-            self.panelTitleLabel.alpha = 1.0
-            self.panelTitleLabel.frame = initialTitleFrame
-            self.panelSeparatorLine.alpha = 1.0
+            self.titleLabel.alpha = 1.0
+            self.titleLabel.frame = initialTitleFrame
+            self.separatorLine.alpha = 1.0
             
-            self.panelHeaderView?.alpha = 1.0
-            self.panelHeaderView?.frame = initialHeaderFrame
+            self.headerView.alpha = 1.0
+            self.headerView.frame = initialHeaderFrame
             }, completion: {finished in
                 UIView.animateWithDuration(0.3) {
-                    self.panelDescriptionLabel.alpha = 1.0
-                    self.panelDescriptionLabel.frame = initialDescriptionFrame
-                    self.panelImageView?.alpha = 1.0
-                    self.panelImageView?.frame = initialImageViewFrame
+                    self.descriptionLabel.alpha = 1.0
+                    self.descriptionLabel.frame = initialDescriptionFrame
+                    self.imageView.alpha = 1.0
+                    self.imageView.frame = initialImageViewFrame
                 }
         })
+    }
+  
+    func buildPanel() {
+        [self, self.headerView, self.titleLabel, self.separatorLine, self.descriptionLabel, self.imageView].map{
+            $0.setTranslatesAutoresizingMaskIntoConstraints(false)
+        }
+
+        self.addSubview(self.headerView)
+        
+        self.titleLabel.numberOfLines = 0
+        
+        self.titleLabel.sizeToFit()
+        
+        self.addSubview(self.titleLabel)
+        self.addSubview(self.separatorLine)
+        
+        self.descriptionLabel.numberOfLines = 0
+        
+        self.descriptionLabel.sizeToFit()
+        
+        self.addSubview(self.descriptionLabel)
+        self.addSubview(self.imageView)
+        
+    }
+    
+    func buildConstraints() {
+        self.updateConstraints()
+    }
+    
+    func centerInSelf(view: UIView) -> NSLayoutConstraint {
+        return NSLayoutConstraint(item: view,
+            attribute: NSLayoutAttribute.CenterX,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self,
+            attribute: NSLayoutAttribute.CenterX,
+            multiplier: 1.0,
+            constant: 0.0)
+    }
+    
+    func pinToLeadingSpace(view: UIView) -> NSLayoutConstraint {
+        return NSLayoutConstraint(item: view,
+            attribute: NSLayoutAttribute.Leading,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self,
+            attribute: NSLayoutAttribute.LeadingMargin,
+            multiplier: 1.0,
+            constant: 0.0)
+    }
+    
+    public override func updateConstraints() {
+        var constraints = [NSLayoutConstraint]()
+
+        var text = "V:|-topPadding-[headerView]-headerTitlePadding-[titleLabel]-separatorPadding-[separatorLine(1.0)]-separatorPadding-[descriptionLabel]-descriptionImagePadding-[imageView]->=0.0-|"
+        
+
+        var views = ["headerView" : self.headerView,
+            "titleLabel" : self.titleLabel,
+            "separatorLine" : self.separatorLine,
+            "descriptionLabel" : self.descriptionLabel,
+            "imageView" : self.imageView]
+        
+        var metrics = ["topPadding" : self.topPadding,
+            "leftRightMargins" : self.leftRightMargins,
+            "bottomPadding" : self.bottomPadding,
+            "headerTitlePadding" : self.headerTitlePadding,
+            "separatorPadding" : self.titleSeparatorPadding,
+            "descriptionImagePadding" : self.descriptionImagePadding]
+
+        NSLayoutConstraint.constraintsWithVisualFormat(text, options: nil, metrics: metrics, views: views).map{
+            constraints.append($0 as NSLayoutConstraint)
+        }
+
+        constraints.append(self.centerInSelf(self.headerView))
+        constraints.append(self.centerInSelf(self.imageView))
+
+        constraints.append(self.pinToLeadingSpace(self.titleLabel))
+        constraints.append(self.pinToLeadingSpace(self.descriptionLabel))
+
+        var textSeparatorLine = "|-[separatorLine]-|"
+
+        NSLayoutConstraint.constraintsWithVisualFormat(textSeparatorLine, options: nil, metrics: nil, views: views).map{
+            constraints.append($0 as NSLayoutConstraint)
+        }
+        
+        self.addConstraints(constraints)
+
+        super.updateConstraints()
+    }
+    
+    public override func intrinsicContentSize() -> CGSize {
+        return self.frame.size
+    }
+    
+    override public class func requiresConstraintBasedLayout() -> Bool {
+        return true
     }
 }
